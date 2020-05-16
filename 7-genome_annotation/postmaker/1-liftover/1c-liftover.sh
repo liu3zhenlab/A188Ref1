ae4c36e00fca74b2d17cd27e1f1cad4378f42d63
Sanzhen Liu <liu3zhen@ksu.edu>
2020-05-16 18:38:24 -0500

    Update 1c-liftover.sh

--- 7-genome_annotation/postmaker/1-liftover/1c-liftover.sh
+++ 7-genome_annotation/postmaker/1-liftover/1c-liftover.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-origff=../../4-makerRUNs/round2/A188r1.maker.output/A188r1.all.makerOnly.gff3
-agp=~/references/A188Ref1/genome/A188Ref1.agp
+origff=A188r1.all.makerOnly.gff3
+agp=A188Ref1.agp
 gff0=A188Ref1a1.0.gff3
 gff1=A188Ref1a1.1.gff3
 
 # liftover
-perl ~/scripts2/maker/gff.agp.liftover.pl --map $agp --gff $origff > $gff0
+perl gff.agp.liftover.pl --map $agp --gff $origff > $gff0
 # sort
-perl /homes/liu3zhen/software/gff3sort/gff3sort.pl --precise --chr_order natural $gff0 > $gff1
+perl gff3sort.pl --precise --chr_order natural $gff0 > $gff1
 # cleanup
 rm $gff0
 
